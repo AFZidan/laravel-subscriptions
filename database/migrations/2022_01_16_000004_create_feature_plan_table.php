1a25ff0a99ab4766966ad13cd20e3cd51545eb6b
Ahmed Zidan <dev.ahmed.zidan@gmail.com>
2023-01-17 14:34:08 +0200

    Fix incompatible foreign key

--- database/migrations/2022_01_16_000004_create_feature_plan_table.php
+++ database/migrations/2022_01_16_000004_create_feature_plan_table.php
@@ -21,20 +21,28 @@ class CreateFeaturePlanTable extends Migration
         }
 
         Schema::create(config('afzidan.subscriptions.tables.feature_plan'), function (Blueprint $table) {
-            $table->foreignId('plan_id')
-                ->constrained(config('afzidan.subscriptions.tables.plans'))
-                ->cascadeOnDelete();
+            $table->integer('plan_id');
+            $table->integer('feature_id');
+            $table->string('value');
+            $table->unsignedSmallInteger('resettable_period')->default(0);
+            $table->string('resettable_interval')->default('month');
+            
+            $table->foreign('plan_id')
+                ->references('id')
+                ->on(config('afzidan.subscriptions.tables.plans'))
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
 
-            $table->foreignId('feature_id')
-                ->constrained(config('afzidan.subscriptions.tables.features'))
-                ->cascadeOnDelete();
+            $table->foreign('feature_id')
+                ->references('id')
+                ->on(config('afzidan.subscriptions.tables.features'))
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
 
             $table->primary(['plan_id', 'feature_id']);
             $table->unique(['plan_id', 'feature_id']);
 
-            $table->string('value');
-            $table->unsignedSmallInteger('resettable_period')->default(0);
-            $table->string('resettable_interval')->default('month');
+
         });
     }
 
